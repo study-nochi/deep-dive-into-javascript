844b7f2e5c489fde09b1bd4ba0ab80322c82ca8e
ainochi-kor <kthrkdals@gmail.com>
2023-05-20 20:22:16 +0900

    wip - callback hell

---
+++ core-javascript/04_Callback_Function/04_callback_hell.js
@@ -0,0 +1,21 @@
+//* 콜백 지옥 예시 (1-1)
+setTimeout(function (name) {
+  var coffeeList = name;
+  console.log(coffeeList)
+
+  setTimeout(function (name) {
+    coffeeList += ', ' + name;
+    console.log(coffeeList)
+
+    setTimeout(function (name) {
+      coffeeList += ', ' + name;
+      console.log(coffeeList)
+      
+      setTimeout(function (name) {
+        coffeeList += ', ' + name;
+        console.log(coffeeList)
+
+      }, 500, '카페라떼');
+    }, 500, '카페모카')
+  }, 500, '아메리카노')
+}, 500, '에스프레소');
