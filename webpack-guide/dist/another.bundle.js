4bac6d96a728f3aa289452adbffb6b4f0fbca33e
ainochi-kor <kthrkdals@gmail.com>
2023-05-21 19:51:13 +0900

    Entry dependencies

---
+++ webpack-guide/dist/another.bundle.js
@@ -0,0 +1,28 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkwebpack_guide"] = self["webpackChunkwebpack_guide"] || []).push([["another"],{
+
+/***/ "./src/another-modules.js":
+/*!********************************!*\
+  !*** ./src/another-modules.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ printMe)\n/* harmony export */ });\nfunction printMe() {\r\n  console.log('I get called from print.js!')\r\n}\n\n//# sourceURL=webpack://webpack-guide/./src/another-modules.js?");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["shared"], () => (__webpack_exec__("./src/another-modules.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
--- webpack-guide/webpack.config.js
+++ webpack-guide/webpack.config.js
@@ -4,25 +4,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js'
+    index: {
+      import: "./src/index.js",
+      dependOn: 'shared',
+    },
+    another: {
+      import: "./src/another-modules.js",
+      dependOn: 'shared'
+    },
+    shared: 'lodash',
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: '.dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    })
-  ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
+    path: path.resolve(__dirname, 'dist')
   },
   optimization: {
-    runtimeChunk: 'single' // 단일 HTML 페이지에 하나 이상의 entry 포인트가 있어서 추가함.
-  }
-}
+    runtimeChunk: 'single',
+  },
+};
