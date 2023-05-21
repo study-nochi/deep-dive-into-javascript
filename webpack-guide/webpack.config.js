df93165be9a906095db4e4e3d55aff4ce2b9b453
ainochi-kor <kthrkdals@gmail.com>
2023-05-21 19:26:16 +0900

    Development

--- webpack-guide/webpack.config.js
+++ webpack-guide/webpack.config.js
@@ -2,18 +2,27 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: '.dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     })
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
+  optimization: {
+    runtimeChunk: 'single' // 단일 HTML 페이지에 하나 이상의 entry 포인트가 있어서 추가함.
+  }
 }
